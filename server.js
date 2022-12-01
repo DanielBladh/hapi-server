307e2437e5f1360056719231126f64169d9eef47
DanielBladh <DanielBladh92@outlook.com>
2022-12-01 14:53:46 +0100

    server updates

--- server.js
+++ server.js
@@ -8,13 +8,33 @@ const init = async () => {
     port: 3001,
   });
 
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: (request, h) => {
-      return "<h1>Hello World!</h1>";
+  server.route([
+    {
+      method: "GET",
+      path: "/",
+      handler: (request, h) => {
+        return "<h1>Hello World!</h1>";
+      },
     },
-  });
+    {
+      method: "GET",
+      path: "/users/{user?}",
+      handler: (request, h) => {
+        if (request.params.soccer) {
+          return `<h1>Hello ${request.params.user}</h1>`;
+        } else {
+          return `<h1>Hello Stranger!</h1>`;
+        }
+      },
+    },
+    {
+      method: "GET",
+      path: "/{any*}",
+      handler: (request, h) => {
+        return "<h1>Oh no! You must be lost!</h1>";
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server started on: ${server.info.uri}`);
